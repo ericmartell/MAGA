a5a3fd7a1245dad69b1aba70e30a7cada8c0b69e
Eric Martell <ericdmartell@gmail.com>
2016-11-09 23:31:02 -0600

    No more reflect asm... doesnt work with private fields.

--- src/com/ericdmartell/maga/utils/ReflectionUtils.java
+++ src/com/ericdmartell/maga/utils/ReflectionUtils.java
@@ -8,15 +8,13 @@ import java.util.Map;
 
 import com.ericdmartell.maga.annotations.MAGAORMField;
 import com.ericdmartell.maga.objects.MAGAObject;
-import com.esotericsoftware.reflectasm.FieldAccess;
 
 import gnu.trove.map.hash.THashMap;
 
 public class ReflectionUtils {
 	// An in-memory cache because inspection is slow.
 
-	private static Map<Class<MAGAObject>, Map<String, Integer>> classesToFieldNamesAndFieldIndex = new THashMap<>();
-	private static Map<Class, FieldAccess> classToFieldAccess = new THashMap<>();
+	private static Map<Class<MAGAObject>, Map<String, Field>> classesToFieldNamesAndFields = new THashMap<>();
 	private static Map<Class<MAGAObject>, Map<String, Class>> classesToFieldNamesAndTypes = new THashMap<>();
 	private static Map<Class, List<String>> indexes = new THashMap<>();
 
@@ -30,26 +28,20 @@ public class ReflectionUtils {
 	}
 
 	public static Class getFieldType(Class clazz, String fieldName) {
+		if (!classesToFieldNamesAndTypes.containsKey(clazz)) {
+			buildIndex(clazz);
+		}
 		return classesToFieldNamesAndTypes.get(clazz).get(fieldName);
 	}
 
 	public static boolean setFieldValue(MAGAObject obj, String fieldName, Object value) {
-		if (!classesToFieldNamesAndFieldIndex.containsKey(obj.getClass())) {
+		if (!classesToFieldNamesAndTypes.containsKey(obj.getClass())) {
 			buildIndex(obj.getClass());
 		}
-		Class fieldClass = classesToFieldNamesAndTypes.get(obj.getClass()).get(fieldName);
-		if (fieldClass == Long.class || fieldClass == long.class) {
-			if (fieldClass == long.class && value == null) {
-				value = 0L;
-			}
-			classToFieldAccess.get(obj.getClass()).set(obj, fieldName, ((Number) value).longValue());
-		} else if (fieldClass == Integer.class || fieldClass == int.class) {
-			if (fieldClass == int.class && value == null) {
-				value = 0;
-			}
-			classToFieldAccess.get(obj.getClass()).set(obj, fieldName, ((Number) value).intValue());
-		} else {
-			classToFieldAccess.get(obj.getClass()).set(obj, fieldName, value);
+		try {
+			classesToFieldNamesAndFields.get(obj.getClass()).get(fieldName).set(obj, value);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			throw new MAGAException(e);
 		}
 
 		return true;
@@ -57,11 +49,16 @@ public class ReflectionUtils {
 	}
 
 	public static Object getFieldValue(MAGAObject obj, String fieldName) {
-		if (!classesToFieldNamesAndFieldIndex.containsKey(obj.getClass())) {
+		if (!classesToFieldNamesAndTypes.containsKey(obj.getClass())) {
 			buildIndex(obj.getClass());
 		}
 
-		Object ret = classToFieldAccess.get(obj.getClass()).get(obj, fieldName);
+		Object ret;
+		try {
+			ret = classesToFieldNamesAndFields.get(obj.getClass()).get(fieldName).get(obj);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			throw new MAGAException(e);
+		}
 		if (ret == null) {
 			return -1L;
 		}
@@ -70,27 +67,22 @@ public class ReflectionUtils {
 	}
 
 	public static List<String> getIndexedColumns(Class clazz) {
-		if (!classesToFieldNamesAndFieldIndex.containsKey(clazz)) {
+		if (!classesToFieldNamesAndTypes.containsKey(clazz)) {
 			buildIndex(clazz);
 		}
 		return indexes.get(clazz);
 	}
 
 	private static void buildIndex(Class clazz) {
-		FieldAccess access = FieldAccess.get(clazz);
-		classToFieldAccess.put(clazz, access);
-
-		Map<String, Integer> fieldNamesToFieldIndex = new THashMap<>();
+		
+		Map<String, Field> fieldNamesToField = new THashMap<>();
 		Map<String, Class> fieldNamesToType = new THashMap<>();
 		List<String> indexedColumns = new ArrayList<>();
 		for (Field field : clazz.getDeclaredFields()) {
-
+			
+			field.setAccessible(true);
 			if (field.isAnnotationPresent(MAGAORMField.class)) {
-				try {
-					fieldNamesToFieldIndex.put(field.getName(), access.getIndex(field.getName()));
-				} catch (RuntimeException e) {
-					
-				}
+				fieldNamesToField.put(field.getName(), field);
 				fieldNamesToType.put(field.getName(), field.getType());
 				MAGAORMField anno = field.getAnnotation(MAGAORMField.class);
 				if (anno.isIndex()) {
@@ -99,10 +91,14 @@ public class ReflectionUtils {
 			}
 		}
 		fieldNamesToType.put("id", Long.class);
-		fieldNamesToFieldIndex.put("id", access.getIndex("id"));
+		try {
+			fieldNamesToField.put("id", clazz.getField("id"));
+		} catch (NoSuchFieldException | SecurityException e) {
+			throw new MAGAException(e);
+		}
 		indexes.put(clazz, indexedColumns);
 
-		classesToFieldNamesAndFieldIndex.put(clazz, fieldNamesToFieldIndex);
+		classesToFieldNamesAndFields.put(clazz, fieldNamesToField);
 		classesToFieldNamesAndTypes.put(clazz, fieldNamesToType);
 	}
 }
